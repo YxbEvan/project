c5b9a267dc3c8d421408b08fd5ad4a20f2b17563
evan yang <fjyangxubin@163.com>
2018-03-19 15:40:44 +0800

    vuex test

---
+++ 01/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- <router-view/> -->
+    <button @click="add">add</button>
+    <button @click="decrease">decrease</button>
+    <button @click="oddAdd">add</button>
+    <div>{{count}}</div>
+    <div>{{getOdd}}</div>
+  </div>
+</template>
+
+<script>
+import {mapGetters, mapActions} from 'vuex'
+export default {
+  name: 'App',
+  computed: mapGetters(['count','getOdd']),
+  methods: mapActions(['add','decrease','oddAdd'])
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 01/src/store.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+let state = {
+  count: 10
+}
+
+const mutations = {
+  add(context){
+    context.count ++
+  },
+  decrease(context){
+    context.count --
+  }
+}
+
+const actions = {
+  add(add){
+    add.commit('add')
+  },
+  decrease(decrease){
+    decrease.commit('decrease')
+  },
+  oddAdd({commit, state}){
+    if(state.count %2 === 0){
+      commit('add')
+    }
+  }
+}
+
+const getters = {
+  count(context){
+    return context.count
+  },
+  getOdd(context){
+    return context.count %2 === 0 ? '偶数' : '奇数'
+  }
+};
+
+export default new Vuex.Store({
+  state,
+  mutations,
+  actions,
+  getters
+})
+
